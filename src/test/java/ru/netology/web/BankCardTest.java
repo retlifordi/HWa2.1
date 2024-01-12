cc40908dcfeb5e895e1e4c1b333ef3559cff3f12
Anastasiia Kosterkina <nastya.kosterkina@mail.ru>
2024-01-12 13:19:55 +0300

    1.2

--- src/test/java/ru/netology/web/BankCardTest.java
+++ src/test/java/ru/netology/web/BankCardTest.java
@@ -45,14 +45,6 @@ class BankCardTest {
         String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
-    @Test
-    void incorrectlyFilledInFullName()  {
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("КостеркинаАнастасия");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79264521842");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
-        assertEquals("Имя и Фамилия указаные неверно", text.trim());
-    }
+
 
 }
